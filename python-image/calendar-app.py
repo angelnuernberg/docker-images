ece8c1fc9c66786529ed230082879190c153f0a7
Angelnuernberg <angelnuernberg@gmail.com>
2024-01-30 17:06:52 +0100

    Python on Docker / dockerfile for python

---
+++ python-image/calendar-app.py
@@ -0,0 +1,6 @@
+import calendar
+print ('Welcome to the calendar application')
+year=int(input('Please enter any year:'))
+month=int(input('Please enter any month:'))
+print(calendar.month(year,month))
+print('Have a nice day')
