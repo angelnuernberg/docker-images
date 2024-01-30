1e52437a0af955b39183802b54dde6d74cc583b0
Angelnuernberg <angelnuernberg@gmail.com>
2024-01-30 23:32:56 +0100

    Added support for CTR+C (SIGINT) at express web server

--- nodejs-image/nodejs-simple-express-web-server/index.js
+++ nodejs-image/nodejs-simple-express-web-server/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+// Package required to handle SIGINT and SIGTERM 
+const process=require('process')
+
 const app = express()
 const port = 3000
+
+// Instructions for processing of SIGINT and SIGTERM:
+// Why is this necessary? -> The app does not finish when Ctrl+C at PowerShell,
+// so to stop the corresponding process   >> docker kill <containerid> is necessary
+// According to node.js doc https://nodejs.org/api/process.html, SIGTERM is not 
+// supported on Windows
+// The express package is already in node_modules, so 'npm install' is not required
+process.on('SIGINT', ()=> {
+    console.log('Application is being interrupted...')
+    process.exit(0)
+})
 
 app.get('/', (req, res) => {
   res.send('This express web application was executed by Docker container!')
